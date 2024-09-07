5e19592748936492992e6321924705d2574ef63f
Mai Xuân Hiếu <maixuanhieu250103@gmail.com>
2024-09-07 14:34:33 +0700

    Demo CICD with CodeBuild and ECR

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const os = require('os')
+
+const HOST = '0.0.0.0'
+const PORT = 8080
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.send(
+        `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
+    );
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`App running on port ${PORT}`);
+})
